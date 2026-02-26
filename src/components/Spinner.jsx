554d7a34297734fd1039a79f04cf1a0b9626aa08
bchethan <157599837+bchethan@users.noreply.github.com>
2026-02-26 18:51:37 +0530

    feat: update Spinner component with color and size props

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,5 +1,5 @@
 import { Spinner } from "flowbite-react";
 
 export function Component() {
-  return <Spinner aria-label="Default status example" />;
+  return <Spinner color="purple" size="xl"/>
 }
